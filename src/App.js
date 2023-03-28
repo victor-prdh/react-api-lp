ecdb3c75571060c9e55ee573f29896cf69b5a2e9
Victor Prudhomme <contact@victor-prdh.com>
2023-03-28 14:34:04 +0200

    feat: basic routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from './Page/Home/Home';
+import LoginPage from './Page/Login/LoginPage';
+
 
 export function RequireAuth({ children }) {
     // Used to ensure the refreshToken is called once at a time
-    const user = ???; // TODO Get user from local storage
+    const user = null; // TODO Get user from local storage
 
     if (user === null) {
         //TODO Navigate to login
@@ -13,11 +17,18 @@ export function RequireAuth({ children }) {
 
 function App() {
 
-//Navigation dans requireAuth
-  return (
-      //TODO ROUTER
-      <div></div>
-  );
+    //Navigation dans requireAuth
+    return (
+        //TODO ROUTER
+        <BrowserRouter>
+            <Routes>
+                <Route path="/">
+                    <Route index element={<HomePage />} />
+                    <Route path="login" element={<LoginPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
