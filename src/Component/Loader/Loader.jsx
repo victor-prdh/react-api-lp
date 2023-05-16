271b96ec474fd71b1d0ebb27a56e2cfc9be4e66b
Victor Prudhomme <contact@victor-prdh.com>
2023-05-16 14:27:11 +0200

    feat: add loader component

---
+++ src/Component/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+import './Loader.css';
+
+const Loader = () => {
+
+    return (
+        <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+    );
+};
+
+export default Loader;
