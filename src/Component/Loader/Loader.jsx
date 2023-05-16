0f17670a7a971facea8d120200b25f07b97cb057
Victor Prudhomme <contact@victor-prdh.com>
2023-05-16 14:58:12 +0200

    fix: react "className"

--- src/Component/Loader/Loader.jsx
+++ src/Component/Loader/Loader.jsx
@@ -6,7 +6,7 @@ import './Loader.css';
 const Loader = () => {
 
     return (
-        <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+        <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
     );
 };
 
